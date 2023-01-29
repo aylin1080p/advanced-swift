a1c6b6e6455751ab3d2d84effaf8852070dee550
aylin gunes <4ylingunes@gmail.com>
2023-01-29 19:00:42 +0100

    Add delay time, generate thread structure

--- advancedSwift.playground/Contents.swift
+++ advancedSwift.playground/Contents.swift
@@ -62,3 +62,58 @@ do {
     print("divided by zero ")
 }
 
+// thread
+
+let queue = DispatchQueue(label: "sticker")
+queue.sync {
+    for i in 100...110 {
+        print("Thread : \(i)")
+    }
+}
+
+for i in 100...110 {
+    print("main loop : \(i)")
+}
+ // we wanted to get them work at the same time
+
+
+let queue2 = DispatchQueue(label: "sticker") // we will say Ascyn !!
+queue.async {
+    for i in 100...110 {
+        print("Thread2 : \(i)")
+    }
+}
+
+for i in 100...110 {
+    print("main loop2 : \(i)")
+}
+// thread has a priority things you can choose one of them
+// usesrInteractive, userInitiated,default,utility,background, unspecified
+
+
+let queue3 = DispatchQueue(label: "sticker3", qos: DispatchQoS.userInitiated) // we will say Ascyn !!
+let queue4 = DispatchQueue(label: "sticker4", qos:DispatchQoS.background)
+
+
+queue3.async {
+    for i in 1...10 {
+        print("Thread3 : \(i)")
+    }
+}
+
+queue4.async {
+    for i in 1...10 {
+        print("Thread4 : \(i)")
+    }
+}
+
+// thread Delay
+
+// seconds,miliseconds...
+
+let queue5 = DispatchQueue(label: "stickerWithDelay",qos: DispatchQoS.userInitiated)
+print(Date())
+var delayTime:DispatchTimeInterval = .seconds(2)
+queue5.asyncAfter(deadline: .now()+delayTime) {
+    print(Date())
+}
